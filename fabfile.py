489dba0887e19af6c492d9e0ba6f60832267117a
Alec Hanefeld <alec@hanefeld.eu>
2014-10-06 19:26:03 +0200

    gunicorn, test in fab work

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,34 @@
-from fabric.api import local
-import restapi.app as app
+from fabric.api import local, parallel, task, serial
+from hinterteil import app
+from multiprocessing import Process
+
+
+def run():
+    #app.run()
+    local('gunicorn -b localhost:5000 hinterteil:app -D -p guni_pid')
+    #return local('python run.py &')
+    #return server
+def kill():
+    local('kill `cat guni_pid`')
 def test():
-    local('echo aa')
-    #app = local('python restapi.py')
-    #local('jamine-node tests/')
-    #app.stop()
+    #run()
+    #local('python run.py', capture=True)
+    #server = Process(target=run)
+    run()
+    try:
+        local('jasmine-node tests/')
+    except:
+        pass
+    finally:
+        kill()
+    #server = local('fab run')
+    #tester = local('fab run_tests')
+    #server.terminate()
+    #run_tests()
+    #server.terminate()
+
+def show_gunis():
+    local('ps -xa | grep gunicorn')
+
+def run_tests():
+    local('jasmine-node tests/')
